e5f3ad0437315b017b4923b3a37bb47c6764944d
houcine <houcinezoghlami@gmail.com>
2023-07-18 00:43:01 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
